1bd191b786cdd5b23b9feb2e26d22025c7d9d64d
Arseniy1315 <90174473+Arseniy1315@users.noreply.github.com>
2021-11-08 13:21:46 +0700

    Add files via upload

---
+++ laba7 JS/script.js
@@ -0,0 +1,50 @@
+
+// let age = prompt('Сколько тебе лет?', 20);
+
+// alert(`Тебе ${age} лет!`); // Тебе 20 лет!
+
+//Выведите на экран любое значение с помощью console.log
+console.log("Кчауууууу");
+
+// Создайте две переменные с числами. Выведите их на экран
+var num = 1231;
+var num2 = 21321; 
+console.log(num , num2);
+
+//Сложите две переменные и выведите результат на экран 
+var a = 5;
+var b = 45;
+var c = a+b;
+console.log(a, b, c);
+
+//Поищите в интернете функции для преобразования строки в верхний и нижний регистр. Выведите на экран два этих значения
+var str = "это молния маккуин";
+var str2 = "УИИИИИИИИИИИИИИ";
+var newstr = str.toUpperCase();
+var newstr2 = str2.toLowerCase();
+console.log(newstr, newstr2);
+
+//Запишите в свой скрипт реализацию каждой базовой операции над числами. Также посмотрите в интернете как возводить число в квадрат, получать остаток от деления
+// Сложение
+var number = 212;
+var number2 = 5;
+console.log(number + number2);
+// Вычитание
+console.log(number - number2);
+//Умножение
+console.log(number * number2);
+//Деление
+console.log(number / number2);
+//Остаток от деления
+console.log(number % number2);
+//Возведение в степень
+console.log(Math.pow(number2, number));
+//Придумайте два задания и покажите преподавателю
+// Функция one * two - (one + two)
+function mult(one, two) { return one * two - (one + two)}
+let x = mult(3, 72);
+console.log(x);
+// Вывод по символьно имени
+for (let char of "Арс") {
+    alert(char);
+}
